d08524d33413eb6ac2aeeba1da6a461778ff182a
thomas <t.kirsch@webcito.de>
2024-03-12 14:26:02 +0100

    Refactor phone input library and update test interface
    The code refactoring includes improvements to jQuery phone input library like addition of country-specific highlighting, and updating the test interface. Changes in the library provide enhanced usability by making it easy for users to identify the default country in the dropdown. Test interface updates involve changes in input ID labels and default values for clearer data interpretation during testing phase.

--- dist/jquery.bsPhone.js
+++ dist/jquery.bsPhone.js
@@ -5,27 +5,58 @@ window.phoneLibrary = [];
 
     const defaultCountry = 'de';
     const wrapperClass = 'js-bs-phone-input';
+    const helperInputClass = 'js-temp-input'
 
-    function setDropDownSelected($input){
-        const inputGroup = $input.closest('.'+wrapperClass);
-        const html = inputGroup.find('.dropdown-item[data-selected="Y"]').html();
-        inputGroup.find('.btn').html( html);
+    $.bsPhone = {
+        setDefaults: function (options = null) {
+            this.DEFAULTS = $.extend({}, this.DEFAULTS, options || {});
+        },
+        getDefaults: function () {
+            return this.DEFAULTS;
+        },
+        DEFAULTS: {
+            default: 'de',
+            wrapperClasses: 'mb-3',
+            dropDownClasses: 'btn btn-outline-secondary',
+            inputClasses: 'border-secondary rounded-end',
+            formatValue(phoneCode, value = '') {
+                return '+' + phoneCode + value;
+            },
+            formatView(phoneCode, value = '') {
+                if (value.startsWith('+' + phoneCode)) {
+                    value = value.replace('+' + phoneCode, '');
+                }
+                return value;
+            }
+        }
+    };
+
+    function setDropDownSelected($input) {
+        console.log($input.attr('id'));
+        const settings = $input.data('options');
+        const inputGroup = $input.closest('.' + wrapperClass);
+        let selected = inputGroup.find('.dropdown-item[data-selected="Y"]');
+        const html = selected.html();
+        inputGroup.find('.dropdown-toggle').html(html);
     }
-    function buildInputGroup($input){
-        if ($input.closest('.'+wrapperClass).lenght){
-            return $input.closest('.'+wrapperClass);
+
+    function buildInputGroup($input) {
+        if ($input.closest('.' + wrapperClass).length) {
+            return $input.closest('.' + wrapperClass);
         }
 
+        const settings = $input.data('options');
+
         const inputGroup = $('<div>', {
-            class: 'input-group '+wrapperClass
+            class: 'input-group ' + wrapperClass + ' ' + settings.wrapperClasses
         }).insertAfter($input);
 
-        const dropDownButton = $('<button>',{
-            class: 'btn btn-outline-secondary dropdown-toggle',
+        const dropDownButton = $('<button>', {
+            class: 'dropdown-toggle ' + settings.dropDownClasses,
             type: 'button',
-            'data-bs-toggle':'dropdown',
+            'data-bs-toggle': 'dropdown',
             'aria-expanded': false,
-            text:''
+            text: ''
         }).appendTo(inputGroup);
 
         const dropDownMenu = $('<div>', {
@@ -41,87 +72,226 @@ window.phoneLibrary = [];
         }).appendTo(dropDownMenu);
 
         window.phoneLibrary.forEach(el => {
-            const activeClass = el.default ? 'Y' : 'N';
-            const a = $('<a>',{
-                class:'dropdown-item',
-                html : `<img src="${el.flag}" style="width: 20px"> +${el.phoneCode}`,
+            const activeClass = settings.default.toUpperCase() === el.isoCode.toUpperCase() ? 'Y' : 'N';
+            const a = $('<a>', {
+                class: 'dropdown-item',
+                html: `<img src="${el.flag}" style="width: 20px"> +${el.phoneCode}`,
                 'data-iso-code': el.isoCode,
-                'data-selected' : activeClass,
+                'data-selected': activeClass,
+                'data-phone-code': el.phoneCode,
                 title: el.country,
-                href:'#'
+                href: '#'
             }).appendTo(dropDownMenu)
         });
 
-        const tmpInput = $('<input>',{
-            type:'tel',
-            class:'js-temp-input form-control'
-        }).appendTo(inputGroup)
-        
-        tmpInput.val($input.val())
 
-        $input.addClass('d-none').appendTo(inputGroup);
+        const tmpInput = $('<input>', {
+            type: 'tel',
+            class: 'form-control ' + helperInputClass + ' ' + settings.inputClasses
+        }).appendTo(inputGroup);
 
-        setDropDownSelected($input);
 
+        $input.attr('type', 'hidden');
+        $input.appendTo(inputGroup);
+
+        setDropDownSelected($input)
         return inputGroup;
     }
 
+    function cleanValue($input) {
+        const settings = $input.data('options');
+        const wrapper = $input.closest('.' + wrapperClass);
+        const helperInput = wrapper.find('.' + helperInputClass);
+        const phoneCode = wrapper.find('.dropdown-item[data-selected="Y"]').data('phoneCode');
+
+        let value = helperInput.val() || '';
+        value = deleteLeadingZeros(value);
+
+        // let eingabe = helperInput.val();
+        // if (eingabe.startsWith('+'+phoneCode)){
+        //     eingabe = eingabe.replace('+'+phoneCode, '');
+        // }
+
+        value = settings.formatValue(phoneCode, value);
+        $input.val(value);
+        helperInput.val(settings.formatView(phoneCode, value));
+
+        console.log(value);
+
+    }
+
     const load = new Promise((resolve, reject) => {
-        if (window.phoneLibraryLoaded){
+        if (window.phoneLibraryLoaded) {
             resolve(window.phoneLibrary);
-        }
-        else {
+        } else {
             loadLibrary(function (array) {
                 resolve(array)
             })
         }
     });
 
-    $.fn.bsPhone = function () {
+    /**
+     *
+     * @param {null|string|object}optionsOrMethod
+     * @return {*|jQuery|HTMLElement}
+     */
+    $.fn.bsPhone = function (optionsOrMethod = null, param = 'undefined') {
+
         if ($(this).length > 1) {
             return $(this).each(function (i, e) {
-                return $(e).bsPhone();
+                return $(e).bsPhone(optionsOrMethod, param);
             })
+        }
+        let callMethod = false;
+        let optionsSet = false;
+
+        switch (typeof optionsOrMethod) {
+            case 'string': {
+                callMethod = true;
+            }
+                break;
+            case 'object': {
+                optionsSet = true;
+                break;
+            }
         }
         const debug = true;
         const input = $(this);
 
-        function init() {
-           load.then((array)=>{
-                const inputGroup = buildInputGroup(input);
-                inputGroup.find('.dropdown-item').on('click', function(){
-                    inputGroup.find('.dropdown-item[data-selected="Y"]').attr('data-selected','N');
-                    $(this).attr('data-selected','Y');
-                    setDropDownSelected(input)
-                });
-               inputGroup.find('[type="search"]').on('keyup input', function () {
-                   const searchPattern = $(this).val().trim();
-                   const searchElements = inputGroup.find('[data-iso-code]');
-                   if (searchPattern !== '') {
-                       searchElements.each(function (index, value) {
 
-                           let currentName = $(value).data('isoCode');
-                           let currentCountry = $(value).attr('title');
-                           if (
-                               currentName.toUpperCase().indexOf(searchPattern.toUpperCase()) > -1 ||
-                               currentCountry.toUpperCase().indexOf(searchPattern.toUpperCase()) > -1
-                           ) {
-                               $(value).removeClass('d-none');
-                           } else {
-                               $(value).addClass('d-none');
-                           }
-                       });
-                   } else {
-                       searchElements.removeClass('d-none');
-                   }
-               })
-            });
-            return input;
+        function init(callback) {
+            load.then((array) => {
+                if (!input.data('initBsPhone')) {
+                    const valueBefore = (input.attr('value') && input.attr('value') !== '') ? input.attr('value') : false;
+                    let setup;
+                    if (!optionsSet) {
+                        setup = $.extend({}, $.bsPhone.getDefaults(), input.data());
+                    } else {
+                        setup = $.extend({}, $.bsPhone.getDefaults(), input.data(), optionsOrMethod || {});
+                    }
+                    input.data('options', setup);
+                    buildInputGroup(input);
+                    events(input);
+                    setDropDownSelected(input);
+                    input.data('initBsPhone', true);
+
+                    if (valueBefore !== false) {
+
+                        setVal(input, valueBefore);
+                    }
+                }
+                return true;
+            }).then(callback);
         }
 
-        init();
+        init(function () {
+            if (callMethod) {
+                switch (optionsOrMethod) {
+                    case 'val':
+                        if (typeof param === 'undefined')
+                            return input.val();
+                        setVal(input, param);
+                }
+            }
+
+            return input;
+        });
+
 
         return input;
+    }
+
+    function setVal($input, value) {
+        const wrapper = $input.closest('.' + wrapperClass);
+        const helperInput = wrapper.find('.' + helperInputClass);
+        if (value) {
+            if (value.startsWith('+')) {
+                const found = window.phoneLibrary.filter(e => {
+                    return value.startsWith('+' + e.phoneCode);
+                });
+                if (found.length) {
+                    value = value.replace('+' + found[0].phoneCode, '');
+                    wrapper.find('.dropdown-item[data-selected="Y"]').attr('data-selected', 'N');
+                    wrapper.find('.dropdown-item[data-phone-code="' + found[0].phoneCode + '"]').attr('data-selected', 'Y');
+
+                }
+            }
+
+
+        }
+
+        helperInput.val(value);
+
+        setDropDownSelected($input)
+        cleanValue($input);
+
+    }
+
+    function deleteLeadingZeros(value = '') {
+        if (value.startsWith('+')) {
+            value = value.substring(1);
+        }
+
+        while (value.startsWith('0')) {
+            value = value.substring(1);
+        }
+        return value;
+    }
+
+    function events($input) {
+        const wrapper = $input.closest('.' + wrapperClass);
+
+        wrapper
+            .on('shown.bs.dropdown', '.dropdown-toggle', function(){
+
+                wrapper.find('[type="search"]').val('').trigger('keyup').focus();
+            })
+            .on('click', '.dropdown-item', function () {
+                wrapper.find('.dropdown-item[data-selected="Y"]').attr('data-selected', 'N');
+                $(this).attr('data-selected', 'Y');
+                setDropDownSelected($input);
+                cleanValue($input);
+            })
+            .on('keyup input', '[type="search"]', function () {
+                const searchPattern = $(this).val().trim();
+                const searchElements = wrapper.find('[data-iso-code]');
+                if (searchPattern !== '') {
+                    searchElements.each(function (index, value) {
+
+                        let currentName = $(value).data('isoCode');
+                        let currentCountry = $(value).attr('title');
+                        if (
+                            currentName.toUpperCase().indexOf(searchPattern.toUpperCase()) > -1 ||
+                            currentCountry.toUpperCase().indexOf(searchPattern.toUpperCase()) > -1
+                        ) {
+                            $(value).removeClass('d-none');
+                        } else {
+                            $(value).addClass('d-none');
+                        }
+                    });
+                } else {
+                    searchElements.removeClass('d-none');
+                }
+            })
+            .on('input keypress', '.' + helperInputClass, function (e) {
+
+                const ok = e.metaKey || // cmd/ctrl
+                    e.which === 43 || // +
+                    e.which === 40 || // (
+                    e.which === 41 || // )
+                    e.which === 45 || // -
+                    e.which === 32 || // space
+                    e.which <= 0 || // arrow keys
+                    e.which == 8 || // delete key
+                    /[0-9]/.test(String.fromCharCode(e.which)); // numbers
+
+                if (!ok) {
+                    e.preventDefault();
+                } else {
+                    cleanValue($input);
+                }
+
+            })
     }
 
 
@@ -136,13 +306,16 @@ window.phoneLibrary = [];
     }
 
 
-
     async function loadLibrary(callback) {
 
         $.getJSON('/dist/countries.json', function (array) {
             array.forEach(country => {
-                const isoToLower = country.isoCode.toLowerCase();
+                let isoToLower = country.isoCode.toLowerCase();
                 country.default = defaultCountry === isoToLower;
+                if(isoToLower === 'an')
+                    isoToLower = 'nl';
+                if(isoToLower === 'uk')
+                    isoToLower = 'sh';
                 country.flag = '/dist/flags/' + isoToLower + '.svg';
             })
             array.sort(sortIsoCodes);
